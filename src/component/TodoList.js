eb4633a6a5d6b212ce4a47a8657814028c2b27f8
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-02-11 15:09:16 +0700

    wow

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,18 +1,71 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { TodoListContext } from '../context/TodoListContext';
 
 const TodoList = () => {
+    // state untuk menampung inputan user
+    const [todo, setTodo] = useState("");
     // memanggil property dari context agar bisa digunakan pada component ini
-    const {todos} = useContext(TodoListContext);
+    const {todos, editId, addTodo, removeTodo, statusTodo, editTodoId, editTodoText, editTodo} = useContext(TodoListContext);
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTodo(todo);
+    };
+
+    const handleRemoveTodo = (todoId) => {
+        removeTodo(todoId);
+    };
+
+    const handleStatus = (todoId) => {
+        statusTodo(todoId);
+    };
+
+    const handleEditId = (todoId) => {
+        editTodoId(todoId);
+    };
+
+    const handleSubmitEdit = (e) => {
+        const text = e.target.value;
+        editTodoText(text);
+    };
+
+    const handleEdit = (todoId) => {
+        editTodo(todoId);
+    };
+
     return(
         <div>
+            <h1>TODO LIST</h1>
             {todos.length ? (
                 todos.map((todo) => {
-                    return <p key={todo.id}>{todo.text}</p>
+                    return <div key={todo.id}>
+                        {editId === todo.id ? (
+                            <input type="text" onChange={handleSubmitEdit} />
+                        ) : (
+                            <h3>{todo.text}</h3>
+                        )}
+                        {editId === todo.id ? (
+                            <button onClick={handleEdit.bind(this,todo.id)}>Submit Edit</button>
+                        ) : (
+                            <button onClick={handleEditId.bind(this,todo.id)}>Edit</button>
+                        )}                        
+                        <button onClick={handleRemoveTodo.bind(this,todo.id)}>Delete</button>
+                        <input type="checkbox" onChange={handleStatus.bind(this,todo.id)} checked={todo.status}/>
+                        <label>{todo.status ? ("Selesai") : ("Belum Selesai")}</label>
+                    </div>
                 })
             ) : (
-                "You Have No Todo!"
+                <h4>You Have No Todo!</h4>
             )}
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="todo">Add Todo</label>
+                <input type="text" onChange={handleChange}/>
+                <button type='submit'>Add New Todo</button>
+            </form>
         </div>
     );
 };
--- src/context/TodoListContext.js
+++ src/context/TodoListContext.js
@@ -4,15 +4,61 @@ export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({children}) => {
     // state yg bisa dipakai secara "global"
-    const [todos, setTodos] = useState([
-        {text: 'Makan', id: 1},
-        {text: 'Main', id: 2},
-        {text: 'Tidur', id: 3}
-    ]);
+    const [todos, setTodos] = useState([]);
+    const [editId, setEditId] = useState(null);
+    const [editText, setEditText] = useState("");
+
+    const addTodo = (todo) => {
+        const newTodo = {
+            text: todo,
+            id: new Date().getTime(),
+            status: false
+        }
+        setTodos([...todos].concat(newTodo));
+    };
+
+    const removeTodo = (todoId) => {
+        const filterTodos = todos.filter((todo) => {
+            return todo.id !== todoId;
+        });
+        setTodos(filterTodos);
+    };
+
+    const statusTodo = (todoId) => {
+        const newStatus = todos.map((todo) => {
+            if(todo.id === todoId){
+                todo.status = !todo.status;
+            }
+            return todo;
+        });
+        setTodos(newStatus);
+        console.log(todos);
+    }
+
+    const editTodoId = (todoId) => {
+        setEditId(todoId);
+    };
+
+    const editTodoText = (text) => {
+        setEditText(text);
+        console.log(editText)
+    };
+
+    const editTodo = (todoId) => {
+        const updatedTodo = todos.map((todo) => {
+            if(todo.id === todoId){
+                todo.text = editText;
+            };
+            return todo;
+        });
+        setTodos(updatedTodo);
+        setEditId(null);
+        setEditText("");
+    }
 
     return (
         // mengikat component children sehingga bisa memakai value yg dipanggil (todos)
-        <TodoListContext.Provider value={todos}>
+        <TodoListContext.Provider value={{todos, editId, addTodo, removeTodo, statusTodo, editTodoId, editTodoText, editTodo}}>
             {children}
         </TodoListContext.Provider>
     )
